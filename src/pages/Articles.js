bbf9c5e5bbb2831ddfdcf1ddbc4c7f1360db0f9c
naimatullahRezaee <naimatullahrezaee@gmail.com>
2023-06-01 16:45:27 +0200

    urls add for each item

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,3 +1,22 @@
-export default function Articles() {
-  return <div>Articles</div>;
+import { useParams } from "react-router-dom";
+import { UseFetch } from "../hooks/UseFetch";
+export default function Home() {
+  const { id } = useParams();
+  const url = "http://localhost:3000/articles/" + id;
+
+  const { data: article, isLoading, error } = UseFetch(url);
+
+  return (
+    <div>
+      {isLoading && <div>Loading... </div>}
+      {error && <div>{error}</div>}
+      {article && (
+        <div>
+          <h2>{article.title}</h2>
+          <p>by: {article.author}</p>
+          <p>{article.body}</p>
+        </div>
+      )}
+    </div>
+  );
 }
